9e8af164d7b37560fec55b3f1f8ce685f6758d8b
Quarx2k <agent00791@gmail.com>
2014-03-14 18:19:11 +0700

    reduce settings.apk size

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+#LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS += --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets --auto-add-overlay
 
 LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
